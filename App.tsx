8e1cba6640f7ca8807ed97683d8d6acf2b67358f
simonesanna1983 <35135396+simonesanna1983@users.noreply.github.com>
2023-04-17 09:54:34 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,66 @@
 import * as React from 'react';
+import Spreadsheet, { RowComponent } from 'react-spreadsheet';
 import './style.css';
 
 export default function App() {
+  // const data = [
+  //   [{ value: "Vanilla" }, { value: "Chocolate" }],
+  //   [{ value: "Strawberry" }, { value: "Cookies" }],
+  // ];
+
+  const initialValue = [
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }, { value: '' }],
+  ];
+
+  const newRow = [
+    { value: '' },
+    { value: '' },
+    { value: '' },
+    { value: '' },
+    { value: '' },
+  ];
+
+  const newColumn = { value: '' };
+
+  const [data, setData] = React.useState(initialValue);
+
+  const addRow = () => {
+    setData([...data, newRow]);
+  };
+
+  //   const addColumn = () => {
+  // data[0] = data[0][];
+  //     setData());
+  //   };
+
+  const toJson = () => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
+  const reset = () => {
+    setData(initialValue);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Spreadsheet data={data} onChange={setData}  />
+      <br />
+      <button onClick={addRow}>Add Row</button>
+      <br /> <br /> <br />
+      {/* <button onClick={addColumn}>Add Column</button> */}
+      <br /> <br /> <br />
+      <button onClick={toJson}>Json</button>
+      <br /> <br /> <br />
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
